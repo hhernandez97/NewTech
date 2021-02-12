d94adc04dd8aad1793d5d00af894032c669b4ab2
Your Name <you@example.com>
2021-02-12 18:05:07 -0400

    NewTech

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+ScrollReveal().reveal('.videogamessc', {delay: 500});
+ScrollReveal().reveal('.bitcoinsc', {delay: 500});
+ScrollReveal().reveal('.coronasc', {delay: 500});
+ScrollReveal().reveal('.businesssc', {delay: 500});
+ScrollReveal().reveal('.technologysc', {delay: 500});
+ScrollReveal().reveal('.socialsc', {delay: 500});
+ScrollReveal().reveal('.worldsc', {delay: 500});
+ScrollReveal().reveal('.extrasc', {delay: 500});
+
